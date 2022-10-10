9fa949854988071ee57dcc3bd1aff2cb08f15185
Andrii <andreikolyckii@gmail.com>
2022-10-10 14:45:20 +0300

    add new sections

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ $(document).ready(function () {
 		$('body').toggleClass('lock')
 	});
 });
+
+//change bg color
+window.onload = function () {
+	var button = document.getElementById('input_button_bg_change');
+	var body = document.getElementsByTagName('body')[0];
+	var colors = ['#ffffff', '#1f2e35'];
+	button.onclick = function () {
+		body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+	};
+};
