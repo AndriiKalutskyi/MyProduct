ae172321bc0712f6f149a6b4991c1ee2d696a7d0
Andrii <andreikolyckii@gmail.com>
2022-10-16 11:20:35 +0300

    add a full page Product

--- js/main.js
+++ js/main.js
@@ -14,3 +14,45 @@ window.onload = function () {
 		body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
 	};
 };
+
+// Carousel
+const owl = $('.owl-carousel');
+
+owl.owlCarousel({
+	loop: true,
+	margin: 20,
+	startPosition: 0,
+	items: 1,
+	responsive: {
+		650: {
+			items: 3,
+			startPosition: 1,
+
+		},
+		1200: {
+			items: 3,
+			margin: 32,
+		},
+	},
+});
+
+$('.slider__btn--prev').click(function () {
+	owl.trigger('prev.owl.carousel');
+});
+
+
+$('.slider__btn--next').click(function () {
+	owl.trigger('next.owl.carousel');
+});
+
+// Nav icon
+const navBtn = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav')
+const menuIcon = document.querySelector('.menu-icon');
+
+
+navBtn.onclick = function () {
+	nav.classList.toggle('nav--mobile')
+	menuIcon.classList.toggle('menu-icon--active');
+	document.body.classList.toggle('no-scroll');
+};
