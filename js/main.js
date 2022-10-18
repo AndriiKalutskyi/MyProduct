9e384137050f63d4bba1dedcd5835ca293ba32a0
Andrii <andreikolyckii@gmail.com>
2022-10-18 15:30:55 +0300

    add new page: customers and pricing

--- js/main.js
+++ js/main.js
@@ -56,3 +56,23 @@ navBtn.onclick = function () {
 	menuIcon.classList.toggle('menu-icon--active');
 	document.body.classList.toggle('no-scroll');
 };
+
+/* Когда пользователь нажимает на кнопку,
+переключение между скрытием и отображением раскрывающегося содержимого */
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Закройте выпадающее меню, если пользователь щелкает за его пределами
+window.onclick = function (event) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
